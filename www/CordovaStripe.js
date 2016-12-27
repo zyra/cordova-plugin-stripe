6d9f4688fbb64c1a3bb9c0c4ab838bf2176dcf4e
Ibby Hadeed <ibby@Ibbys-Mac.local>
2016-12-27 04:47:41 -0500

    fix JS interface

--- www/CordovaStripe.js
+++ www/CordovaStripe.js
@@ -1,11 +1,12 @@
 var exec = require('cordova/exec');
 
 function trimArgs(args) {
+    args = Array.from(args);
     args.splice(args.length-2, 2);
     return args;
 }
 
-exports = {
+module.exports = {
 
     setPublishableKey: function(key, success, error) {
         exec(success, error, "CordovaStripe", "setPublishableKey", trimArgs(arguments));
