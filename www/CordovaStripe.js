e707726026c008609e758742aead83ce84aba832
Ibby <ibby93@gmail.com>
2016-12-27 06:50:27 -0500

    add readme

--- www/CordovaStripe.js
+++ www/CordovaStripe.js
@@ -8,10 +8,22 @@ function trimArgs(args) {
 
 module.exports = {
 
+    /**
+     * Set publishable key
+     * @param key {string} Publishable key
+     * @param success {Function} Success callback
+     * @param error {Function} Error callback
+     */
     setPublishableKey: function(key, success, error) {
         exec(success, error, "CordovaStripe", "setPublishableKey", trimArgs(arguments));
     },
 
+    /**
+     * Create a credit card token
+     * @param creditCard {Object} Credit card information
+     * @param success {Function} Success callback
+     * @param error {Function} Error callback
+     */
     createCardToken: function(creditCard, success, error) {
         exec(success, error, "CordovaStripe", "createCardToken", trimArgs(arguments));
     }
