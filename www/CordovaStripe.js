a918762b19dce5d62e148d1f39e700dd78b11153
Ibby Hadeed <ibby@Ibbys-Mac.local>
2016-12-27 05:37:23 -0500

    remove validation functions

--- www/CordovaStripe.js
+++ www/CordovaStripe.js
@@ -12,18 +12,6 @@ module.exports = {
         exec(success, error, "CordovaStripe", "setPublishableKey", trimArgs(arguments));
     },
 
-    validateCardNumber: function(cardNumber, success, error) {
-        exec(success, error, "CordovaStripe", "validateCardNumber", trimArgs(arguments));
-    },
-
-    validateExpiry: function(month, year, success, error) {
-        exec(success, error, "CordovaStripe", "validateExpiry", trimArgs(arguments));
-    },
-
-    validateCVC: function(cvc, success, error) {
-        exec(success, error, "CordovaStripe", "validateCVC", trimArgs(arguments));
-    },
-
     createCardToken: function(creditCard, success, error) {
         exec(success, error, "CordovaStripe", "createCardToken", trimArgs(arguments));
     }
