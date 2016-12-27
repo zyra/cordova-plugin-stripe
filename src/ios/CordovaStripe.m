23e22d7206c06b2ce7aef8738cd41c35aa922291
Ibby Hadeed <ibby@Ibbys-Mac.local>
2016-12-27 04:16:52 -0500

    update framework

--- src/ios/CordovaStripe.m
+++ src/ios/CordovaStripe.m
@@ -6,22 +6,23 @@
 - (void)setPublishableKey:(CDVInvokedUrlCommand*)command
 {
     NSString* publishableKey = [[command arguments] objectAtIndex:0];
-    [[STPPaymentConfiguration sharedConfiguration] setPublishableKey: publishableKey;
+    NSLog(@"Setting publishable key to %@", publishableKey);
+    [[STPPaymentConfiguration sharedConfiguration] setPublishableKey:publishableKey];
     CDVPluginResult* result = [CDVPluginResult
-                                resultWithStatus: CDVCommandStatus_OK];
+                               resultWithStatus: CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 - (void)greet:(CDVInvokedUrlCommand*)command
 {
-
+    
     NSString* name = [[command arguments] objectAtIndex:0];
     NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
-
+    
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:msg];
-
+    
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
