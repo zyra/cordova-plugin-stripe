5dca8ef83dcdfa909989f5cd64e5192f25bbf8d5
Christopher Woolum <chris@waxonmobile.com>
2017-08-20 21:16:34 -0700

    add typings for more stuff

--- typings/CordovaStripe.d.ts
+++ typings/CordovaStripe.d.ts
@@ -1,12 +1,96 @@
+
+interface Window {
+    plugin: any;
+}
+
 interface CordovaPlugins {
     stripe: ICordovaStripe;
 }
 
 interface ICordovaStripe {
-    createCardToken: (card: IStripeCreateCardTokenRequest, token: (token: IStripeToken) => void, err: (err) => void) => void;
+
+    /**
+    * Create a credit card token
+    * @param creditCard {module:stripe.CreditCardTokenParams} Credit card information
+    * @param success {Function} Success callback
+    * @param error {Function} Error callback
+    */
+    createCardToken: (card: ICreateStripeCardTokenRequest, success: (token: IStripeToken) => void, err: (err) => void) => void;
+
+    /**
+     * Set publishable key
+     * @param key {string} Publishable key
+     * @param [success] {Function} Success callback
+     * @param [error] {Function} Error callback
+     */
+    setPublishableKey: (key: string, success?: () => void, error?: (err) => void) => void;
+
+    /**
+     * Create a bank account token
+     * @param bankAccount {module:stripe.BankAccountTokenParams} Bank account information
+     * @param {Function} success Success callback
+     * @param {Function} error Error callback
+     */
+    createBankAccountToken: (bankAccount: ICreateStripeBankAccountTokenRequest, success: (bankAccount: ICreateStripeBankAccountTokenResponse) => void, error: (err) => void) => void;
+
+    /**
+     * Validates card number
+     * @param cardNumber {String} Credit card number
+     * @param {Function} success  Success callback that will be called if card number is valid
+     * @param {Function} error  Error callback that will be called if card number is invalid
+     */
+    validateCardNumber: (cardNumber, success: () => void, error: () => void) => void;
+
+    /**
+     * Validates the expiry date of a card
+     * @param {number} expMonth Expiry month
+     * @param {number} expYear Expiry year
+     * @param {Function} success
+     * @param {Function} error
+     */
+    validateExpiryDate: (expMonth: number, expYear: number, success: () => void, error: () => void) => void;
+
+    /**
+     * Validates a CVC of a card
+     * @param {string} cvc CVC/CVV
+     * @param {Function} success
+     * @param {Function} error
+     * @example
+     * function onSuccess() {
+     *   console.log('isValid');
+     * }
+     *
+     * function onError() {
+     *   console.log('invalid');
+     * }
+     *
+     * cordova.plugin.stripe.validateCVC('424', onSuccess, onError);
+     */
+    validateCVC: (cvc: string, success: () => void, error: () => void) => void;
+
+    /**
+     * Gets a card type from a card number
+     * @param {string} cardNumber Credit card number
+     * @param {Function} success
+     * @param {Function} error
+     * @example
+     * cordova.plugins.stripe.getCardType('4242424242424242', function(cardType) {
+     *   console.log(cardType); // visa
+     * });
+     */
+    getCardType: (cardNumber: string, success: () => void, error: (err) => void) => void;
 }
 
-interface IStripeCreateCardTokenRequest {
+interface ICreateStripeBankAccountTokenRequest {
+    country: string,
+    currency: string,
+    account_holder_name: string,
+    account_holder_type: string,
+    routing_number: string,
+    account_number: stirng
+}
+
+interface ICreateStripeCardTokenRequest {
     number: string,
     expMonth: number,
     expYear: number,
@@ -57,5 +141,27 @@ interface IStripeCardResponse {
     tokenization_method?: any;
 }
 
-interface Metadata {
+export interface StripeBankAccount {
+    id: string;
+    object: string;
+    account_holder_name: string;
+    account_holder_type: string;
+    bank_name: string;
+    country: string;
+    currency: string;
+    fingerprint: string;
+    last4: string;
+    routing_number: string;
+    status: string;
+}
+
+export interface ICreateStripeBankAccountTokenResponse {
+    id: string;
+    object: string;
+    bank_account: StripeBankAccount;
+    client_ip?: any;
+    created: number;
+    livemode: boolean;
+    type: string;
+    used: boolean;
 }
